5e6f6342346b047d50fcc374eff9586950aa13b4
Oscar Romero <creaturenex@gmail.com>
2021-03-18 17:35:19 -0400

    Completed RB1390 Medium_2 practice problems

--- Problems/RB130_Ruby_Foundations/Medium_2/test_text.rb
+++ Problems/RB130_Ruby_Foundations/Medium_2/test_text.rb
@@ -4,12 +4,13 @@ require_relative 'text'
 
 class TextTest < Minitest::Test
   def setup
-    @text_file = File.open('/home/oscar/LaunchSchool/LS_Courses/Problems/RB130_Ruby_Foundations/Medium_2/sample_text.txt', "r")
+    @text_file = File.open('sample_text.txt', "r")
     # this can be written with the block { |file| file.read }, when the method exits the block it will automatically close the file.
   end
 
   def teardown
-  @text_file.close
+    @text_file.close
+    puts "File has been closed: #{@text_file.closed?}"
   end
 
   def test_swap
@@ -26,5 +27,20 @@ class TextTest < Minitest::Test
     TEXT
 
     assert_equal expected_text, actual_text
+  end
+
+  def test_word_count
+    text = Text.new(@text_file.read)
+
+    expected_text = <<~TEXT
+    Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
+    Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
+    quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo
+    nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum
+    dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
+    et verius meuris, et pheretre mi.
+    TEXT
+
+    assert_equal expected_text.split.count, text.word_count
   end
 end
